31506ae1a96b536ef70e0af9b2be41a2de27ad39
Francis Levesque <flevesqu@cisco.com>
2020-04-06 11:51:52 +0200

    Build out views for login partial and main chat page

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.smack
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -29,5 +30,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginButtonNavClicked(view: View) {
+
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMessageButtonClicked(view: View) {
+        
     }
 }
