68303665a7588112e41efc6c4e6a228d6afd14b8
Francis Levesque <flevesqu@cisco.com>
2020-04-07 12:08:35 +0200

    Create login activity

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -33,7 +34,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginButtonNavClicked(view: View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View) {
