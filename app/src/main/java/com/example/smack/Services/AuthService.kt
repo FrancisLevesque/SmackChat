77d4a9e5d65ec26a0ade11f9691390b95b18c7d3
Francis Levesque <flevesqu@cisco.com>
2020-04-13 10:22:15 +0200

    Add login request

--- app/src/main/java/com/example/smack/Services/AuthService.kt
+++ app/src/main/java/com/example/smack/Services/AuthService.kt
@@ -2,29 +2,34 @@ package com.example.smack.Services
 
 import android.content.Context
 import android.util.Log
+import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.HttpHeaderParser
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.android.volley.toolbox.*
+import com.example.smack.Utilities.URL_LOGIN
 import com.example.smack.Utilities.URL_REGISTER
+import org.json.JSONException
 import org.json.JSONObject
 
 
 object AuthService {
+
+    var isLoggedIn = false
+    var userEmail = ""
+    var authToken = ""
+
     fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
         jsonBody.put("password", password)
-
         val requestBody = jsonBody.toString()
 
-        val registerRequest = object : StringRequest(Method.POST, URL_REGISTER, Response.Listener { response ->
+        val registerRequest = object: StringRequest(Method.POST, URL_REGISTER, Response.Listener { response ->
             println(response)
             complete(true)
         }, Response.ErrorListener { error ->
-            val response = error.networkResponse
-            val message = String(response.data)
-            Log.d("ERROR", "Could not register user: $message")
+//            val response = error.networkResponse
+//            val message = String(response.data)
+            Log.d("ERROR", "Could not register user: $error")
             complete(false)
         }) {
             override fun getBodyContentType(): String {
@@ -37,5 +42,37 @@ object AuthService {
         }
 
         Volley.newRequestQueue(context).add(registerRequest)
+    }
+
+    fun loginUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("password", password)
+        val requestBody = jsonBody.toString()
+
+        val loginRequest = object: JsonObjectRequest(Method.POST, URL_LOGIN, null, Response.Listener { response ->
+            try {
+                userEmail = response.getString("user")
+                authToken = response.getString("token")
+                isLoggedIn = true
+                complete(true)
+            } catch (e: JSONException) {
+                Log.d("JSON EXCEPTION", e.localizedMessage)
+                complete(false)
+            }
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR", "Could not login user: $error")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestBody.toByteArray()
+            }
+        }
+
+        Volley.newRequestQueue(context).add(loginRequest)
     }
 }
