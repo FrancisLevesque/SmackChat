8b0c128bc8fcd8b4efe6cb1b58a7a0a4a59069f3
Francis Levesque <flevesqu@cisco.com>
2020-04-19 13:39:38 +0200

    Selectable channels

--- app/src/main/java/com/example/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/smack/Controller/LoginActivity.kt
@@ -26,7 +26,7 @@ class LoginActivity : AppCompatActivity() {
 
         if (email.isNotEmpty() && password.isNotEmpty()) {
             enableSpinner(true)
-            AuthService.loginUser(this, email, password) { loginSuccess ->
+            AuthService.loginUser(email, password) { loginSuccess ->
                 if (loginSuccess) {
                     AuthService.findUserByEmail(this) { findSuccess ->
                         if (findSuccess) {
--- app/src/main/java/com/example/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controller/MainActivity.kt
@@ -24,12 +24,14 @@ import com.example.smack.Utilities.SOCKET_URL
 import io.socket.client.IO
 import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val socket = IO.socket(SOCKET_URL)
     lateinit var channelAdapter: ArrayAdapter<Channel>
+    var selectedChannel : Channel? = null
 
     private fun setupAdapters() {
         channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, MessageService.channels)
@@ -52,6 +54,12 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         setupAdapters()
+
+        channel_list.setOnItemClickListener { _, _, position, _ ->
+            selectedChannel = MessageService.channels[position]
+            drawer_layout.closeDrawer(GravityCompat.START)
+            updateWithChannel()
+        }
 
         if (App.prefs.isLoggedIn) {
            AuthService.findUserByEmail(this) {}
@@ -81,13 +89,22 @@ class MainActivity : AppCompatActivity() {
                 userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
                 loginButtonNavHeader.text = "Logout"
 
-                MessageService.getChannels(context) { complete ->
+                MessageService.getChannels { complete ->
                     if(complete) {
+                        if (MessageService.channels.count() > 0) {
+                            selectedChannel = MessageService.channels[0]
+                            channelAdapter.notifyDataSetChanged()
+                            updateWithChannel()
+                        }
                         channelAdapter.notifyDataSetChanged()
                     }
                 }
             }
         }
+    }
+
+    fun updateWithChannel() {
+        mainChannelName.text = "#${selectedChannel?.name}"
     }
 
     override fun onBackPressed() {
@@ -118,7 +135,7 @@ class MainActivity : AppCompatActivity() {
             val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
 
             builder.setView(dialogView)
-                .setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
+                .setPositiveButton("Add") { _, _ ->
                     val nameTextField = dialogView.findViewById<EditText>(R.id.addChannelNameText)
                     val descriptionTextField = dialogView.findViewById<EditText>(R.id.addChannelDescriptionText)
                     val channelName = nameTextField.text.toString()
@@ -126,7 +143,7 @@ class MainActivity : AppCompatActivity() {
 
                     socket.emit("newChannel", channelName, channelDescription)
                 }
-                .setNegativeButton("Cancel") { dialog: DialogInterface?, which: Int ->
+                .setNegativeButton("Cancel") { _, _ ->
                 }
                 .show()
         }
